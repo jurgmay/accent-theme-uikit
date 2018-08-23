84453821ccecc48410eff4ab6ce7a6586e426075
Juergen May <j.may@accent.uk.com>
2018-08-23 13:21:47 +0100

    procuctCtrl - Revert changes

--- app/js/controllers/productCtrl.js
+++ app/js/controllers/productCtrl.js
@@ -163,7 +163,7 @@ function ($scope, $routeParams, $route, $location, $451, Product, ProductDisplay
 	
 	$scope.showErrorModal = function() {
 
-		showError();	
+		UIkit.modal.dialog('<p>UIkit dialog!</p>');	
 
 		// your logic goes here
 		return true;
