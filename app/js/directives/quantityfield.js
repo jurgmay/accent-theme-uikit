597efa7606d37af617cf31c8882b2e5d5d2e50d1
Juergen May <j.may@accent.uk.com>
2018-09-02 14:16:26 +0100

    quantityfield - UIkit update to make the field xsmall

--- app/js/directives/quantityfield.js
+++ app/js/directives/quantityfield.js
@@ -8,7 +8,7 @@ four51.app.directive('quantityfield', ['$451', 'ProductDisplayService', function
         restrict: 'E',
         template: '<div>'+
             '<inlineerror ng-show="lineitem.qtyError" title="{{lineitem.qtyError}}" />'+
-            '<select id="451qa_input_qty" class="uk-select uk-form-small uk-form-width-small" ng-change="qtyChanged(lineitem)" ng-if="lineitem.PriceSchedule.RestrictedQuantity" ng-required="required" ng-model="lineitem.Quantity" ng-options="pb.Quantity as getRestrictedQtyText(pb, lineitem.Product.QuantityMultiplier) for pb in lineitem.PriceSchedule.PriceBreaks" ui-validate="\'validQuantityAddToOrder($value, lineitem)\'"><option value=""></option></select>'+
+            '<select id="451qa_input_qty" class="uk-select uk-form-small uk-form-width-xsmall" ng-change="qtyChanged(lineitem)" ng-if="lineitem.PriceSchedule.RestrictedQuantity" ng-required="required" ng-model="lineitem.Quantity" ng-options="pb.Quantity as getRestrictedQtyText(pb, lineitem.Product.QuantityMultiplier) for pb in lineitem.PriceSchedule.PriceBreaks" ui-validate="\'validQuantityAddToOrder($value, lineitem)\'"><option value=""></option></select>'+
             '<input ng-disabled="lineitem.Variant.RecordCount > 0 && lineitem.ID" id="451qa_input_qty" placeholder="0" autocomplete="off" class="uk-form-small uk-form-width-small" ng-change="qtyChanged(lineitem)" ng-if="!lineitem.PriceSchedule.RestrictedQuantity" type="text" ng-required="required" name="qtyInput" ng-model="lineitem.Quantity" ui-validate="\'validQuantityAddToOrder($value, lineitem)\'"/>'+
             '</div>',
         link: function(scope){
