90c73b1eabc9392446c1e17d910f2eb43a82f004
Juergen May <j.may@accent.uk.com>
2019-03-16 15:18:46 +0000

    Revert "UIkit changes"
    This reverts commit b54aab07ab69c8d87b4af32d6f448d52738633e7.

--- app/js/controllers/specFormCtrl.js
+++ app/js/controllers/specFormCtrl.js
@@ -12,8 +12,6 @@ function ($scope, $location, $route, $routeParams, $window, ProductDisplayServic
     else {init()}
 
     function init() {
-    flatpickr("#testme");
-
         $scope.variantErrors = [];
         var varID = $routeParams.variantInteropID == 'new' ? null :  $routeParams.variantInteropID;
         $scope.loadingImage = true;
