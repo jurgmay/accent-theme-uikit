b54aab07ab69c8d87b4af32d6f448d52738633e7
Juergen May <j.may@accent.uk.com>
2019-03-16 14:54:37 +0000

    UIkit changes

--- app/js/controllers/specFormCtrl.js
+++ app/js/controllers/specFormCtrl.js
@@ -12,6 +12,8 @@ function ($scope, $location, $route, $routeParams, $window, ProductDisplayServic
     else {init()}
 
     function init() {
+    flatpickr("#testme");
+
         $scope.variantErrors = [];
         var varID = $routeParams.variantInteropID == 'new' ? null :  $routeParams.variantInteropID;
         $scope.loadingImage = true;
